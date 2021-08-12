a0017cc88e5d0ed4a956b4a839b678930d95d0a9
Julio Cesar Barboza Filho <54064857+Julio-Cesar123@users.noreply.github.com>
2021-08-12 09:58:03 -0300

    Update Domino.c

--- codigos/Domino.c
+++ codigos/Domino.c
@@ -64,22 +64,22 @@ int main() {
 
 
 void Menu() { //hub do sistema
-
-    printf("\n    █████████████████ \t\t      ***********************                   █████████████████");
-    printf("\n    ██             ██ \t\t\t     Bem-Vindo                          ██             ██");
-    printf("\n    ██  O       O  ██ \t\t\t       ao                               ██  O       O  ██");
-    printf("\n    ██             ██ \t\t\t     Meu Jogo                           ██             ██");
-    printf("\n    ██  O       O  ██ \t\t      ***********************                   ██  O       O  ██");
-    printf("\n    ██             ██ \t\t\t  -=--=DOMINO=--=-                      ██             ██");
-    printf("\n    ██  O       O  ██     _________________________________________________     ██  O       O  ██");
-    printf("\n    ██▄▄▄▄▄▄▄▄▄▄▄▄▄██ \t\t >Pressione  <1> para embaralhar as pecas       ██▄▄▄▄▄▄▄▄▄▄▄▄▄██");
-    printf("\n    ██             ██ \t\t                                                ██             ██");
-    printf("\n    ██  O       O  ██ \t\t >Pressione  <2> para organizar as pecas        ██  O       O  ██");
-    printf("\n    ██             ██ \t\t                                                ██             ██");
-    printf("\n    ██  O       O  ██ \t\t >Pressione  <3> para mostrar as pecas          ██  O       O  ██");
-    printf("\n    ██             ██                                                           ██             ██");
-    printf("\n    ██  O       O  ██ \t\t >Pressione  <4> para sair                      ██  O       O  ██");
-    printf("\n    █████████████████     _________________________________________________     █████████████████");
+        
+    printf("\n    _________________ \t\t      ***********************                   _________________");
+    printf("\n    ||             || \t\t\t     Bem-Vindo                          ||             ||");
+    printf("\n    ||  O       O  || \t\t\t       ao                               ||  O       O  ||");
+    printf("\n    ||             || \t\t\t     Meu Jogo                           ||             ||");
+    printf("\n    ||  O       O  || \t\t      ***********************                   ||  O       O  ||");
+    printf("\n    ||             || \t\t\t  -=--=DOMINO=--=-                      ||             ||");
+    printf("\n    ||  O       O  ||     _________________________________________________     ||  O       O  ||");
+    printf("\n    ||_____________|| \t\t >Pressione  <1> para embaralhar as pecas       ||_____________||");
+    printf("\n    ||             || \t\t                                                ||             ||");
+    printf("\n    ||  O       O  || \t\t >Pressione  <2> para organizar as pecas        ||  O       O  ||");
+    printf("\n    ||             || \t\t                                                ||             ||");
+    printf("\n    ||  O       O  || \t\t >Pressione  <3> para mostrar as pecas          ||  O       O  ||");
+    printf("\n    ||             ||                                                           ||             ||");
+    printf("\n    ||  O       O  || \t\t >Pressione  <4> para sair                      ||  O       O  ||");
+    printf("\n    ||_____________||     _________________________________________________     ||_____________||");
 }
 
 void LimparInterface(){ //limpa o sistema
