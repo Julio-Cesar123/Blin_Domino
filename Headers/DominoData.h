f8f5e94227baa0d968c6c4a68f68972d05af4f79
Julio Cesar Barboza Filho <54064857+Julio-Cesar123@users.noreply.github.com>
2021-09-23 10:51:58 -0300

    Add files via upload

---
+++ Headers/DominoData.h
@@ -0,0 +1,11 @@
+#ifndef DOMINODATA_H_
+#define DOMINODATA_H_
+
+#include "DominoModel.h"
+
+void salvarArquivo(tipo_jogo domino[], tipo_peca jogador1[], tipo_peca jogador2[], tipo_mesa tabuleiro[], tipo_peca monte[]);
+
+void carregarArquivo(tipo_jogo domino[], tipo_peca jogador1[], tipo_peca jogador2[], tipo_mesa tabuleiro[], tipo_peca monte[]);
+
+
+#endif
